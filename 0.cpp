85a0cc99b37cf81cde1d46302ba275eddf5a2cbe
ms3b <90153608+ms3bs@users.noreply.github.com>
2021-12-24 06:13:26 +0300

    Add files via upload

---
+++ 0.cpp
@@ -0,0 +1,13 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//    int n;
+//    long double a, b;
+//    a = 45.1657*1000;
+//    n = 692;
+//    cout << a << endl;
+//    cout << int(a * n);
+//    return 0;
+//}
