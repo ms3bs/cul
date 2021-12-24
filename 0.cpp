ed84741956b2aaacc13a0182c85770bb0cbd074b
ms3b <90153608+ms3bs@users.noreply.github.com>
2021-12-24 06:16:51 +0300

    Update 0.cpp

--- 0.cpp
+++ 0.cpp
@@ -1,13 +1,7 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<iostream>
-//using namespace std;
-//int main()
-//{
-//    int n;
-//    long double a, b;
-//    a = 45.1657*1000;
-//    n = 692;
-//    cout << a << endl;
-//    cout << int(a * n);
-//    return 0;
-//}
+#include<iostream>
+using namespace std;
+int main()
+{
+  cout<<123;
+  return 0;
+}
